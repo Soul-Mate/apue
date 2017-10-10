bf337cc2aa5f281dfae14071f012c05144854220
Yuanchang.xu <yuanchang.xu@outlook.com>
2017-10-10 18:31:45 +0800

    signal

--- unit10/kill.c
+++ unit10/kill.c
@@ -8,8 +8,6 @@ int
 main(int argc, char const *argv[])
 {
 	pid_t pid;
-
-	
 	if ((pid = fork()) < 0)
 		err_sys("fork error");
 	else if(pid == 0){
--- unit10/sigjmp.c
+++ unit10/sigjmp.c
@@ -12,12 +12,22 @@ static volatile sig_atomic_t canjump;
 int
 main(int argc, char const *argv[])
 {
-	struct sigaction act, oact;
+	struct sigaction act, oact, saveusr1, osaveusr1;
 	act.sa_flags = 0;
 	sigemptyset(&act.sa_mask);
 	act.sa_handler = sig_int;
 	if (sigaction(SIGINT, &act, &oact) < 0)
-		err_sys("sigaction error");
+		err_sys("sigaction SIGINT error");
+	saveusr1.sa_flags = 0;
+	saveusr1.sa_handler = sig_usr;
+	sigemptyset(&saveusr1.sa_mask);
+	if (sigaction(SIGUSR1, &saveusr1, &osaveusr1) < 0)
+		err_sys("sigaction SIGUSR error");
+	if(sigsetjmp(jmpbuf, 1)){
+		puts("sigsetjmb ending main");
+		exit(0);
+	}
+	canjump = 1;
 	for(;;)
 		pause();
 	return 0;
@@ -26,11 +36,18 @@ main(int argc, char const *argv[])
 static void
 sig_int(int signo)
 {
+	puts("");
 	puts("signal caught SIGINT");
 }
 
 static void
 sig_usr(int signo)
 {
-
+	if (canjump == 0)
+		return;
+	if (signo == SIGUSR1){
+		printf("signal caught SIGUSR1\n");
+		siglongjmp(jmpbuf,1);
+		canjump = 0;
+	}
 }
