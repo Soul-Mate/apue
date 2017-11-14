fc1763dc5453383474447b9eddabecaade67f5a2
Yuanchang <yuanchang.xu@outlook.com>
2017-11-14 21:44:35 +0800

    merge

--- unit11/deadlock2.c
+++ unit11/deadlock2.c
@@ -21,10 +21,34 @@ int
 main(int argc, char const *argv[])
 {	
 
+	int err;
+	pthread_t tid1, tid2;
 	lock_list * locks;
 	locks = locks_alloc();
 	if (locks == NULL) {
 		printf("%s\n", "locks_alloc error");
+		exit(1);
+	}
+	/* create thread 1 */
+	err = pthread_create(&tid1, NULL, start_routine1, (void *)locks);
+	if (err != 0) {
+		printf("%s\n", "pthread_create error");
+		exit(1);
+	}
+	/* create thread 2 */
+	err = pthread_create(&tid2, NULL, start_routine2, (void *)locks);
+	if (err != 0) {
+		printf("%s\n", "pthread_create error");
+		exit(1);
+	}
+	err = pthread_join(tid1,NULL);
+	if (err != 0) {
+		printf("%s\n", "pthread_detach error");
+		exit(1);
+	}
+	err = pthread_join(tid2,NULL);
+	if (err != 0) {
+		printf("%s\n", "pthread_detach error");
 		exit(1);
 	}
 	return 0;
@@ -42,4 +66,46 @@ locks_alloc()
 		return NULL;
 	}
 	return locks;
+}
+
+void *
+start_routine1(void *arg)
+{
+	int err;
+	lock_list * locks = (lock_list *)arg;
+	/* hold mutex1 lock mutex 2 */
+	printf("%s\n", "thread 1 lock mutex1");
+	err = pthread_mutex_lock(&locks->lock1);
+	if (err != 0) {
+		printf("%s\n", "pthread_mutex_lock error");
+		exit(1);
+	}
+	printf("%s\n", "thread 1 lock mutex2");
+	err = pthread_mutex_lock(&locks->lock2);
+	if (err != 0) {
+		printf("%s\n", "pthread_mutex_lock error");
+		exit(1);
+	}
+	return (void *)NULL;
+}
+
+void *
+start_routine2(void *arg)
+{
+	int err;
+	lock_list * locks = (lock_list *)arg;
+	/* hold mutex1 lock mutex 2 */
+	printf("%s\n", "thread 2 lock mutex2");
+	err = pthread_mutex_lock(&locks->lock2);
+	if (err != 0) {
+		printf("%s\n", "pthread_mutex_lock error");
+		exit(1);
+	}
+	printf("%s\n", "thread 2 lock mutex1");
+	err = pthread_mutex_lock(&locks->lock1);
+	if (err != 0) {
+		printf("%s\n", "pthread_mutex_lock error");
+		exit(1);
+	}
+	return (void *)NULL;
 }
